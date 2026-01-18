42d1a3a1fea26899ad46b5e6cc1e00f0e570dafe
Jiayue Qin <jiayueq@outlook.com>
2026-01-18 21:37:00 +0800

    Add new arguments for PCA and embedding options

--- nutkin/parser.py
+++ nutkin/parser.py
@@ -35,23 +35,24 @@ def parse_arg():
     parser = argparse.ArgumentParser(description="Run Nutkin variability analysis.")
     parser.add_argument("--input_path", type=str, required=True, help="Path to input file (.h5ad, .csv, or 10X folder)")
     parser.add_argument("--output_path", type=str, required=True,help="Path to save processed AnnData file (.h5ad)")
-    parser.add_argument("--skip_preprocessing", action="store_true", help="Path to input file (.h5ad, .csv, or 10X folder)")
+    parser.add_argument("--n_comps", type=int, default=50, help="Number of PCA components in scree plot")
+    parser.add_argument("--run_pca", action="store_true", help="Whether to compute and plot PCA scree plot")
+    parser.add_argument("--run_embedding", action="store_true", help="Whether to compute and plot low-dimensional embedding (e.g. UMAP)")
     parser.add_argument("--embedding_method", choices=["umap", "phate", "densmap"], default="umap",
                         help="Embedding method to use")
-    parser.add_argument("--color", nargs="+", default="group",
-                        help="Metadata column(s) in adata.obs used to color the plot")
+    parser.add_argument("--group_col", type=str, default="group",
+                        help="Metadata column(s) in adata.obs used to color the plot and group")
     parser.add_argument("--use_rep", default="X_pca", help="Representation to use for embedding (e.g., X_pca or X)")
-    parser.add_argument("--save_scree_plot", action="store_true", help="Save PCA variance explained plot")
-    parser.add_argument("--save_embedding_plot", action="store_true", help="Save embedding plot as PNG")
-    parser.add_argument("--n_comps", type=int, default=30, help="Number of PCA components")
+
+
     
-    
-    parser.add_argument("--group_col", type=str, default="cell_type", help="Column name in obs for grouping")
     parser.add_argument("--group1",nargs='+', default=None, help="Group 1 name (optional)")
     parser.add_argument("--group2",nargs='+', default=None, help="Group 2 name (optional)")
     parser.add_argument("--metric_type", choices=["sum", "product", "both"], default='sum', help="Type of metric to test: 'sum', 'product', or 'both")
+    parser.add_argument("--variability_method", choices=["sd", "mad", "both"], default='mad', help="Type of metric to calculate variability: 'standard deviation', 'median absolute deviation', or 'both")
     parser.add_argument("--detail", action="store_true", help="Detailed PCA visualization")
     parser.add_argument("--plot_pc_num", default=3, help="Number of PCs to plot")
+    parser.add_argument("--plot_type", choices=["scatter", "contour"], default='scatter', help="Type of PCA visualization: 'scatter', 'contour'")
     return parser.parse_args()
     
     
