92a36e59cbc87d56eccdb6ebc8a3974bd6613056
Jiayue Qin <jiayueq@outlook.com>
2026-01-18 21:36:04 +0800

    Update main.py

--- nutkin/main.py
+++ nutkin/main.py
@@ -32,22 +32,27 @@ def main():
 
     # === Step 1: Load and preprocess the data ===
     logger.info("Loading and preprocessing data...")
-    adata = load_data(args.input_path) 
-    data_pre = preprocess_adata(
-            adata,n_comps=args.n_comps,
-            save_scree_plot=args.save_scree_plot,skip_preprocessing=args.skip_preprocessing,output_path=args.output_path)
-    plot_embedding(data_pre,method=args.embedding_method,color=args.color,use_rep=args.use_rep,
-    save_embedding_plot=args.save_embedding_plot,output_path=args.output_path)
+    adata = load_data(
+        data_path=args.input_path,
+        n_comps=args.n_comps,
+        output_path=args.output_path,
+        run_pca=True,
+        run_embedding=args.run_embedding,
+        method=args.embedding_method
+    )
+
     
    
    
     # === Step 2: Initialize Nutkin ===
     logger.info("Initializing Nutkin...")
     processor = Nutkin(
-        adata=data_pre,
+        adata=adata,
         group_col=args.group_col,
         output_dir=args.output_path,
         num_pc=DEFAULT_NUM_PC,
+        metric_type=args.metric_type,
+        variability_method=args.variability_method,
         verbal=True,
         detail=args.detail
     )
@@ -60,7 +65,7 @@ def main():
     # === Step 4: Perform differential testing ===
     logger.info("Performing pairwise differential tests and plotting results...")
     processor.differential_test(group1=args.group1,group2=args.group2)
-    processor.visualize_pca_results(detail=args.detail,plot_pc_num=args.plot_pc_num)
+    processor.visualize_pca_results(detail=args.detail,plot_pc_num=args.plot_pc_num,plot_type=args.plot_type)
    
 
 if __name__ == "__main__":
