189bb9ce560f0d1ab1c0f10683ee71bf756b1c0e
Jiayue Qin <jiayueq@outlook.com>
2026-01-19 16:39:06 +0800

    Add logging and visualization features to main.py

--- nutkin/main.py
+++ nutkin/main.py
@@ -41,6 +41,49 @@ def main():
         method=args.embedding_method
     )
 
+    from matplotlib import pyplot as plt
+import logging
+import textwrap
+from preprocessing import *
+from config import *
+from parser import parse_arg
+from nutkin import Nutkin
+import pandas as pd
+
+# setup logging
+LOG_FORMAT = \
+'(%(asctime)s) %(message)s'
+DATE_FORMATE = '%d/%m/%Y %H:%M:%S' #'%a, %d %b %Y %H:%M:%S'
+logging.basicConfig(format=LOG_FORMAT, datefmt=DATE_FORMATE)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+def print_logo(args):
+  
+    print(textwrap.dedent(
+            f'''\n\nWelcome to \n
+                {NUTKIN_LOGO}
+        '''))
+    return
+
+
+
+def main():
+    args = parse_arg()
+    print_logo(args)
+
+    # === Step 1: Load and preprocess the data ===
+    logger.info("Loading and preprocessing data...")
+    adata = load_data(
+        data_path=args.input_path,
+        n_comps=args.n_comps,
+        output_path=args.output_path,
+        run_pca=True,
+        run_embedding=args.run_embedding,
+        method=args.embedding_method
+    )
+
     
    
    
@@ -49,7 +92,7 @@ def main():
     processor = Nutkin(
         adata=adata,
         group_col=args.group_col,
-        output_dir=args.output_path,
+        output_path=args.output_path,
         num_pc=DEFAULT_NUM_PC,
         metric_type=args.metric_type,
         variability_method=args.variability_method,
